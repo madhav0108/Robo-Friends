0308540e54fb87549428e1156c633edb1bac242f
Madhav Sharma <madhava010801@gmail.com>
2024-01-22 13:33:01 -0800

    fixed unique key prop warning

--- src/CardList.js
+++ src/CardList.js
@@ -4,7 +4,7 @@ import Card from './Card.js';
 const CardList = ({robots}) => {
     const cardArr = robots.map((user, i) => {
         return (
-        <Card key={i.id} 
+        <Card key={user.id} 
             id={robots[i].id} 
             name={robots[i].name} 
             email={robots[i].email}
