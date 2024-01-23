eda9e47459ae98f8fb50e653d3673be7b25a88a6
Madhav Sharma <madhava010801@gmail.com>
2024-01-23 11:39:33 -0800

    folder structure and code clean up

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
-import Scroll from './Scroll';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
 
 class App extends React.Component {
     constructor() {
@@ -27,8 +27,9 @@ class App extends React.Component {
     }
 
     render() {
-        const filteredRobots = this.state.robots.filter(robots => {
-            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        const {robots, searchfield} = this.state;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         })
         return (
             <div className='tc'>
