2269414e064efa6ffea27598e7aaa27c55c73fe7
Madhav Sharma <madhava010801@gmail.com>
2024-02-19 10:45:05 -0800

    complete

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,7 +4,7 @@ import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundary';
 
-function App() {
+function App() { //class App extends React.Component {
     // constructor() {
     //     super()
     //     this.state = { robots: [], searchfield: '' }
@@ -12,6 +12,7 @@ function App() {
 
     const [robots, setRobots] = useState([])
     const [searchfield, setSearchfield] = useState('')
+    const [count, setCount] = useState(0)
 
     // componentDidMount() {
     //     fetch('https://jsonplaceholder.typicode.com/users')
@@ -23,18 +24,20 @@ function App() {
         fetch('https://jsonplaceholder.typicode.com/users')
         .then(response=> response.json())
         .then(users=> {setRobots(users)})
-    },[])
+    },[count]) // only run and fetch if count changes
 
     const onSearchChange = (event) => {
         setSearchfield(event.target.value)
     }
 
+    // render() {
     const filteredRobots = robots.filter(robot => {
         return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     })
     return (
         <div className='tc'>
             <h1>RoboFriends</h1>
+            <button onClick={()=>setCount(count+1)}>Click Me!</button>
             <SearchBox searchChange = {onSearchChange}/>
             <Scroll>
                 <ErrorBoundary>
